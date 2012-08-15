935d76af0564cf9d7064ca01a4032821a2b810cd
Yu Fu <yufu@thoughtworks.com>
2012-08-15 17:53:06 +0100

    added amend page and enable to create an amending content related ticket

--- lib/app.rb
+++ lib/app.rb
@@ -17,10 +17,24 @@ class App < Sinatra::Base
     erb :new, :locals => {:departments => departments}
   end
 
+  get '/amend' do
+    departments = ZendeskHelper.get_departments
+    erb :amend, :locals => {:departments => departments}
+  end
+
   post '/new' do
     comment = params[:target_url] + "\n\n" + params[:new_content] + "\n\n" + params[:content_additional]
     subject = "New Content"
     tag = "new_content"
+    ZendeskHelper.raise_zendesk_request(subject, tag, params[:name], params[:email], params[:department], params[:job_title], params[:phone_number], params[:content_added_date],params[:not_added_before], comment)
+    puts params, comment, subject, tag
+    redirect '/acknowledge'
+  end
+
+  post '/amend' do
+    comment = params[:target_url] + "\n\n" + "[old content]\n" + params[:old_content] + "\n\n" + "[new content]\n"+params[:new_content] + "\n\n" + params[:place_to_remove] + "\n\n" + params[:additional]
+    subject = "Amend Content"
+    tag = "amend_content"
     ZendeskHelper.raise_zendesk_request(subject, tag, params[:name], params[:email], params[:department], params[:job_title], params[:phone_number], params[:content_added_date],params[:not_added_before], comment)
     puts params, comment, subject, tag
     redirect '/acknowledge'
