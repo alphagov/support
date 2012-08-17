c61a86ca954ba0fd6553f7eb81a50d922788b80c
Seema Satish <seema.satish@digital.cabinet-office.com>
2012-08-17 12:13:11 +0100

    Added "Create User" functionality
    @author Yu
    @author Seema

--- lib/app.rb
+++ lib/app.rb
@@ -16,18 +16,18 @@ class App < Sinatra::Base
   end
 
   get '/new' do
-    departments = ZendeskClient.get_departments
-    erb :new, :locals => {:departments => departments}
+    @departments = ZendeskClient.get_departments
+    erb :new
   end
 
   get '/amend' do
-    departments = ZendeskClient.get_departments
-    erb :amend, :locals => {:departments => departments}
+    @departments = ZendeskClient.get_departments
+    erb :amend
   end
 
   get '/delete' do
-    departments = ZendeskClient.get_departments
-    erb :delete, :locals => {:departments => departments}
+    @departments = ZendeskClient.get_departments
+    erb :delete
   end
 
   post '/new' do
@@ -35,7 +35,7 @@ class App < Sinatra::Base
     comment = url + "\n\n" + params[:new_content] + "\n\n" + params[:additional]
     subject = "New Content"
     tag = "new_content"
-    ZendeskClient.raise_zendesk_request(subject, tag, params[:name], params[:email], params[:department], params[:job], params[:phone], params[:need_by],params[:not_before], comment)
+    ZendeskClient.raise_zendesk_request(subject, tag, params[:name], params[:email], params[:department], params[:job], params[:phone], comment, params[:need_by],params[:not_before])
     redirect '/acknowledge'
   end
 
@@ -44,7 +44,7 @@ class App < Sinatra::Base
     comment = url + "\n\n" + "[old content]\n" + params[:old_content] + "\n\n" + "[new content]\n"+params[:new_content] + "\n\n" + params[:place_to_remove] + "\n\n" + params[:additional]
     subject = "Amend Content"
     tag = "amend_content"
-    ZendeskClient.raise_zendesk_request(subject, tag, params[:name], params[:email], params[:department], params[:job], params[:phone], params[:need_by],params[:not_before], comment)
+    ZendeskClient.raise_zendesk_request(subject, tag, params[:name], params[:email], params[:department], params[:job], params[:phone], comment, params[:need_by],params[:not_before])
     redirect '/acknowledge'
   end
 
@@ -53,7 +53,24 @@ class App < Sinatra::Base
     comment = url + "\n\n" + params[:additional]
     subject = "Delete Content"
     tag = "delete_content"
-    ZendeskClient.raise_zendesk_request(subject, tag, params[:name], params[:email], params[:department], params[:job], params[:phone], params[:need_by],"", comment)
+    ZendeskClient.raise_zendesk_request(subject, tag, params[:name], params[:email], params[:department], params[:job], params[:phone], comment, params[:need_by],nil)
     redirect '/acknowledge'
   end
+
+
+#  User access routing
+
+  get '/create-user' do
+    @departments = ZendeskClient.get_departments
+    erb :user
+  end
+
+  post '/create-user' do
+    subject = "Create New User"
+    tag = "new_user"
+    comment = params[:user_name] + "\n\n" + params[:user_email]+ "\n\n" + params[:additional]
+    ZendeskClient.raise_zendesk_request(subject, tag, params[:name], params[:email], params[:department], params[:job], params[:phone], comment, nil, nil)
+    redirect '/acknowledge'
+  end
+
 end
