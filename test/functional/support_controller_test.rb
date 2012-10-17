5259e315b106897a3a44d19129c938bcb0e1d10a
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2012-10-17 15:33:10 +0000

    Test SupportController#campaigns

--- test/functional/support_controller_test.rb
+++ test/functional/support_controller_test.rb
@@ -232,4 +232,73 @@ class SupportControllerTest < ActionController::TestCase
       assert_redirected_to "/acknowledge"
     end
   end
+
+  context "GET campaign" do
+    setup do
+      stub_zendesk_organisation_list
+    end
+
+    should "render the form" do
+      get :campaign
+      assert_select "h1", /Campaign/i
+    end
+
+    should "use ZenDesk to populate the organisation dropdown" do
+      get :campaign
+      assert_select "select#organisation_list option", "Advocate General for Scotland"
+    end
+  end
+
+  context "POST campaign" do
+    setup do
+      stub_zendesk_organisation_list
+    end
+
+    should "reject invalid requests" do
+      params = {
+        "name"=>"Testing", 
+        "email"=>"testing@digital.cabinet-office.gov.uk", 
+        "job"=>"doo", 
+        "phone"=>"", 
+        "organisation"=>"", 
+        "other_organisation"=>"", 
+        "campaign_name"=>"Testing", 
+        "erg_number"=>"1234", 
+        "start_day"=>"", 
+        "start_month"=>"", 
+        "start_year"=>"", 
+        "description"=>"Testing", 
+        "company"=>"", 
+        "url"=>"", 
+        "additional"=>""
+      }
+      post :campaign, params
+      assert_response 200 # should actually be an error status, but let's worry about that later
+      assert_template "campaigns/campaign"
+      assert_select ".errors", /Organisation information is required/
+    end
+
+    should "submit it to ZenDesk" do
+      params = {
+        "name"=>"Testing", 
+        "email"=>"testing@digital.cabinet-office.gov.uk", 
+        "job"=>"doo", 
+        "phone"=>"", 
+        "organisation"=>"cabinet_office", 
+        "other_organisation"=>"", 
+        "campaign_name"=>"Testing", 
+        "erg_number"=>"1234", 
+        "start_day"=>"", 
+        "start_month"=>"", 
+        "start_year"=>"", 
+        "description"=>"Testing", 
+        "company"=>"", 
+        "url"=>"", 
+        "additional"=>""
+      }
+      ZendeskRequest.expects(:raise_zendesk_request).returns("not a null")
+      post :campaign, params
+      assert_redirected_to "/acknowledge"
+    end
+  end  
 end
