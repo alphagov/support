2c31fc8f0779592e4a059d729a6848833b27e0f1
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2012-10-17 15:44:44 +0000

    Test SupportController#publish_tool

--- test/functional/support_controller_test.rb
+++ test/functional/support_controller_test.rb
@@ -300,8 +300,7 @@ class SupportControllerTest < ActionController::TestCase
       post :campaign, params
       assert_redirected_to "/acknowledge"
     end
-  end  
-
+  end
   
   context "GET general" do
     setup do
@@ -356,5 +355,60 @@ class SupportControllerTest < ActionController::TestCase
       post :general, params
       assert_redirected_to "/acknowledge"
     end
-  end  
+  end
+
+  context "GET publish_tool" do
+    setup do
+      stub_zendesk_organisation_list
+    end
+
+    should "render the form" do
+      get :publish_tool
+      assert_select "h1", /Publishing Tool/i
+    end
+
+    should "use ZenDesk to populate the organisation dropdown" do
+      get :publish_tool
+      assert_select "select#organisation_list option", "Advocate General for Scotland"
+    end
+  end
+
+  context "POST publish_tool" do
+    setup do
+      stub_zendesk_organisation_list
+    end
+
+    should "reject invalid requests" do
+      params = {
+        "name"=>"Testing", 
+        "email"=>"testing@digital.cabinet-office.gov.uk", 
+        "job"=>"dev", 
+        "phone"=>"", 
+        "organisation"=>"", 
+        "other_organisation"=>"", 
+        "url"=>"testing", 
+        "additional"=>""
+      }
+      post :publish_tool, params
+      assert_response 200 # should actually be an error status, but let's worry about that later
+      assert_template "tech-issues/publish_tool"
+      assert_select ".errors", /Organisation information is required/
+    end
+
+    should "submit it to ZenDesk" do
+      params = {
+        "name"=>"Testing", 
+        "email"=>"testing@digital.cabinet-office.gov.uk", 
+        "job"=>"dev", 
+        "phone"=>"", 
+        "organisation"=>"cabinet_office", 
+        "other_organisation"=>"", 
+        "url"=>"testing", 
+        "additional"=>""
+      }
+      ZendeskRequest.expects(:raise_zendesk_request).returns("not a null")
+      post :publish_tool, params
+      assert_redirected_to "/acknowledge"
+    end
+  end
 end
