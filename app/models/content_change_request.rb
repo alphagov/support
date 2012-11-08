b64ad0feba7b1333bd9cedc0d7d4b76a35ff4121
Jake Benilov <benilov@gmail.com>
2012-11-08 17:49:41 +0000

    removing unused model code for now

--- app/models/content_change_request.rb
+++
@@ -1,7 +0,0 @@
-require 'support_request'
-
-class ContentChangeRequest < SupportRequest
-  attr_accessor :add_content
-
-  validates_presence_of :add_content
-end
--- test/unit/models/content_change_request_test.rb
+++
@@ -1,5 +0,0 @@
-require 'test_helper'
-
-class ContentChangeRequestTest < Test::Unit::TestCase
-  should validate_presence_of(:add_content)
-end
