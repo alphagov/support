a2afa2599240992f07d252b74478322d49b5ef68
Jake Benilov <benilov@gmail.com>
2012-11-09 16:22:35 +0000

    small tweak to application controller to remove duplication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,24 +11,14 @@ class ApplicationController < ActionController::Base
   private
 
   def on_get(template)
-    begin
-      @client = ZendeskClient.get_client(logger)
-      @organisations = ZendeskRequest.get_organisations(@client)
-    rescue ZendeskError
-      return render :"support/zendesk_error",  :locals => {:error_string => "zendesk_error_upon_new_form"}
-    end
+    load_client_and_organisations("zendesk_error_upon_new_form")
 
     @formdata = {}
     render :"#{template}", :layout => "application"
   end
 
   def on_post(params, route)
-    begin
-      @client = ZendeskClient.get_client(logger)
-      @organisations = ZendeskRequest.get_organisations(@client)
-    rescue ZendeskError
-      return render :"support/zendesk_error", :locals => {:error_string => "zendesk_error_upon_submit"}
-    end
+    load_client_and_organisations("zendesk_error_upon_submit")
     @formdata = params
 
     if @errors.empty?
@@ -40,6 +30,15 @@ class ApplicationController < ActionController::Base
       end
     else
       render :"#{@template}", :layout => "application", :status => 400
+    end
+  end
+
+  def load_client_and_organisations(error_string)
+    begin
+      @client = ZendeskClient.get_client(logger)
+      @organisations = ZendeskRequest.get_organisations(@client)
+    rescue ZendeskError
+      return render :"support/zendesk_error", :locals => {:error_string => error_string}
     end
   end
 
