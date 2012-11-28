1dcc209901d7a3d0e31f93535d6c157e338fe60e
Jake Benilov <benilov@gmail.com>
2012-11-28 09:55:29 +0000

    removing unnecessary request context option, reorder options

--- app/models/with_request_context.rb
+++ app/models/with_request_context.rb
@@ -19,10 +19,9 @@ module WithRequestContext
 
   def request_context_options
     [
-      ["Mainstream (business/citizen)", "mainstream"],
-      ["Inside Government", "inside_government"],
       ["Detailed Guidance", "detailed_guidance"],
-      ["Other/Not sure", "other"]
+      ["Inside Government", "inside_government"],
+      ["Mainstream (business/citizen)", "mainstream"],
     ]
   end
 end
