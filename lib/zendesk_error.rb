005f19b617676efe3f5b7836ef155f3628703c63
Jake Benilov <benilov@gmail.com>
2012-11-29 16:36:55 +0000

    added a mechanism to create and update users in zendesk

--- lib/zendesk_error.rb
+++ lib/zendesk_error.rb
@@ -1,10 +1,1 @@
-class ZendeskError < StandardError
-
-  attr_reader :message, :details_from_zendesk
-
-  def initialize(message, details_from_zendesk)
-    @message = message.to_s
-    @details_from_zendesk = details_from_zendesk
-  end
-
-end
+class ZendeskError < StandardError; end
