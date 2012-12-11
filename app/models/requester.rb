bfc13a658a4f7cd370e1c7511ce601f51aa4f467
Jake Benilov <benilov@gmail.com>
2012-12-11 14:54:01 +0000

    removing redundant attr_accessor

--- app/models/requester.rb
+++ app/models/requester.rb
@@ -1,7 +1,7 @@
 require 'tableless_model'
 
 class Requester < TablelessModel
-  attr_accessor :email, :collaborator_emails
+  attr_accessor :email
 
   validates_presence_of :email
   validates :email, :format => {:with => /@/}
