0745cccb407a8a0c24ceb019a71a48ee8f264b8c
Jake Benilov <benilov@gmail.com>
2012-12-12 09:56:24 +0000

    better factoring of CreateNewUserRequestsControllerTest

--- test/functional/create_new_user_requests_controller_test.rb
+++ test/functional/create_new_user_requests_controller_test.rb
@@ -11,12 +11,14 @@ class CreateNewUserRequestsControllerTest < ActionController::TestCase
 
   context "submitted user creation request" do
     should "submit it to ZenDesk" do
-      params = valid_create_new_user_request_params
-
-      post :create, params
+      post :create, valid_create_new_user_request_params
 
       assert_equal ['new_user'], @zendesk_api.ticket.tags
       assert_redirected_to "/acknowledge"
+    end
+
+    should "create a Zendesk user with the requested user details" do
+      post :create, valid_create_new_user_request_params
 
       expected_created_user_attributes = {
         email: "subject@digital.cabinet-office.gov.uk",
