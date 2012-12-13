05c914b2dd50eba3340ad3283de4c5610c02b860
Jake Benilov <benilov@gmail.com>
2012-12-13 13:53:44 +0000

    storing user info from SSO in the Rails cache

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,25 @@
 require 'gds-sso/user'
 
-class ReadOnlyUser < OpenStruct
+class User < OpenStruct
   def self.attr_accessible(*args)
   end
 
   include GDS::SSO::User
 
   def self.find_by_uid(uid)
-    ReadOnlyUser.new(uid: uid)
+    auth_hash = Rails.cache.fetch(uid)
+    auth_hash ? User.new(auth_hash) : nil
   end
 
   def self.create!(auth_hash, options={})
-    ReadOnlyUser.new(auth_hash)
+    Rails.cache.write(auth_hash["uid"], auth_hash)
+    User.new(auth_hash)
   end
 
   def update_attribute(*args)
   end
 
   def update_attributes(params, hash)
-    ReadOnlyUser.new(params)
+    User.new(params)
   end
 end
