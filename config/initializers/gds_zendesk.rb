5f481e9e5935d5299be6c42b29bd9d210f386516
Jake Benilov <benilov@gmail.com>
2013-01-17 12:00:14 +0000

    simplifying the initializer

--- config/initializers/gds_zendesk.rb
+++ config/initializers/gds_zendesk.rb
@@ -5,14 +5,7 @@ require 'gds_zendesk/dummy_client'
 GDS_ZENDESK_CLIENT = if Rails.env.development? || Rails.env.test?
   GDSZendesk::DummyClient.new(logger: Rails.logger)
 else
-  username, password = nil
   config_yaml_file = File.join(Rails.root, 'config', 'zendesk.yml')
-  if File.exist?(config_yaml_file)
-    config = YAML.load_file(config_yaml_file)[Rails.env]
-    unless config.nil?
-      username = config['username']
-      password = config['password']
-    end
-  end
-  GDSZendesk::Client.new(username: username, password: password, logger: Rails.logger)
+  config = YAML.load_file(config_yaml_file)[Rails.env]
+  GDSZendesk::Client.new(username: config['username'], password: config['password'], logger: Rails.logger)
 end
