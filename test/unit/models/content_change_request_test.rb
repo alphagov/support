02731cc3b88e6b2e869a837425a7286097a0fe37
Jake Benilov <benilov@gmail.com>
2013-01-28 16:52:22 +0000

    test for nil title for content change requests

--- test/unit/models/content_change_request_test.rb
+++ test/unit/models/content_change_request_test.rb
@@ -7,6 +7,8 @@ class ContentChangeRequestTest < Test::Unit::TestCase
   should validate_presence_of(:request_context)
 
   should allow_value("xxxx").for(:title)
+  should allow_value(nil).for(:title)
+  
   should allow_value("https://www.gov.uk").for(:url)
   should allow_value("https://www.gov.uk/A\nhttps://www.gov.uk/A").for(:related_urls)
 
