da4f0de9eb8816ab0932ca45e9060bff529edaca
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-03-13 13:01:13 +0000

    Allow Signon permission/sign out push API to work
    This works in production because this file is overwritten with one which
    contains these credentials.

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -4,4 +4,6 @@ GDS::SSO.config do |config|
   config.oauth_secret = 'secret'
   config.default_scope = "Support"
   config.oauth_root_url = Plek.current.find("signon")
+  config.basic_auth_user = 'api'
+  config.basic_auth_password = 'defined_on_rollout_not'
 end
