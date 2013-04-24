8e247d9b85130f12eecce95b7d78ecc981f98cf6
Jake Benilov <benilov@gmail.com>
2013-04-24 14:42:33 +0000

    bugfix: user can have multiple permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,14 +3,9 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    case
-    when user.has_permission?('single_points_of_contact')
-      can :manage, :all
-    when user.has_permission?('campaign_requesters')
-      can :manage, CampaignRequest
-    when user.has_permission?('content_requesters')
-      can :manage, [ NewFeatureRequest, ContentChangeRequest ]
-    end
+    can :manage, :all if user.has_permission?('single_points_of_contact')
+    can :manage, CampaignRequest if user.has_permission?('campaign_requesters')    
+    can :manage, [ NewFeatureRequest, ContentChangeRequest ] if user.has_permission?('content_requesters')
 
     can :manage, [GeneralRequest, AnalyticsRequest]
   end
