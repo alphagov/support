8e4ff638b7dff1f9dbbec7e5f3193acbbe81476b
Jake Benilov <benilov@gmail.com>
2013-06-12 19:17:09 +0000

    refactor user facing component class

---
+++ lib/support/gds/user_facing_components.rb
@@ -0,0 +1,29 @@
+require 'support/gds/user_facing_component'
+
+module Support
+  module GDS
+    class UserFacingComponents
+      class << self
+        def all
+          component_options.map do |options|
+            UserFacingComponent.new(options)
+          end
+        end
+
+        def find(attributes)
+          all.detect { |component| component.id == attributes["name"] }
+        end
+
+        protected
+        def component_options
+          [
+            { name: "GOV.UK itself", id: "gov_uk" },
+            { name: "Inside Government Publisher", id: "inside_government_publisher", inside_government_related: true },
+            { name: "Mainstream Publisher", id: "mainstream_publisher" },
+            { name: "Travel Advice Publisher", id: "travel_advice_publisher" },
+          ]
+        end
+      end
+    end
+  end
+end
