ae0c3fad376caaea619f55459673caa17c21f61c
Jake Benilov <benilov@gmail.com>
2013-06-21 14:54:11 +0000

    extend generator with controller, view, cucumber

--- test/generators/request_generator_test.rb
+++ test/generators/request_generator_test.rb
@@ -8,11 +8,36 @@ class RequestGeneratorTest < Rails::Generators::TestCase
   setup :prepare_destination
   teardown :remove_temp_files
 
-  test 'all files are properly created' do
-    run_generator %w{ my_thing }
-    assert_file 'lib/support/requests/my_thing_request.rb', /My thing/
-    assert_file 'lib/support/requests/my_thing_request.rb', /class MyThingRequest/
+  setup do
+    run_generator %w{ my_thing }    
+  end
+
+  test 'model files are created' do
+    assert_file 'lib/support/requests/my_thing_request.rb',
+      /My thing/,
+      /class MyThingRequest/
+
     assert_file 'test/unit/support/requests/my_thing_request_test.rb', /MyThingRequestTest/
+  end
+
+  test 'controller created' do
+    assert_file 'app/controllers/my_thing_requests_controller.rb',
+      /class MyThingRequestsController/
+  end
+
+  test 'zendesk ticket model file created' do
+    assert_file 'lib/zendesk/ticket/my_thing_request_ticket.rb',
+      /class MyThingRequestTicket/
+  end
+
+  test 'view templates created' do
+    assert_file 'app/views/my_thing_requests/new.html.erb', /My Thing/
+    assert_file 'app/views/my_thing_requests/_request_details.html.erb'
+  end
+
+  test 'cucumber code created' do
+    assert_file 'features/my_thing_requests.feature', /Feature: My thing requests/
+    assert_file 'features/step_definitions/my_thing_requests_steps.rb'
   end
 
   def remove_temp_files
