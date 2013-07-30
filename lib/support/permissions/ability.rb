cfdf317e68ca31609948434ce71550f5f7a797d6
Jake Benilov <benilov@gmail.com>
2013-07-30 15:35:32 +0000

    make 'unpublish content' form available to all
    after a lengthy discussion with the IG team, the decision was taken
    to allow all users to remove the restriction of who can submit
    'unpublish content' requests, with the possibility of tightening
    the permissions later if it becomes a problem.

--- lib/support/permissions/ability.rb
+++ lib/support/permissions/ability.rb
@@ -10,11 +10,11 @@ module Support
       def initialize(user)
         can :manage, :all if user.has_permission?('single_points_of_contact')
         can :manage, CampaignRequest if user.has_permission?('campaign_requesters')    
-        can :manage, [ NewFeatureRequest, ContentChangeRequest, UnpublishContentRequest ] if user.has_permission?('content_requesters')
+        can :manage, [ NewFeatureRequest, ContentChangeRequest ] if user.has_permission?('content_requesters')
         can :manage, [ CreateOrChangeUserRequest, RemoveUserRequest ] if user.has_permission?('user_managers')
         can :manage, [ ErtpProblemReport ] if user.has_permission?('ertp')
 
-        can :manage, [GeneralRequest, AnalyticsRequest, TechnicalFaultReport]
+        can :manage, [GeneralRequest, AnalyticsRequest, TechnicalFaultReport, UnpublishContentRequest]
       end
     end
   end
