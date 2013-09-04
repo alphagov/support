885b07df4808ad1f5f9c31cb639382e79750ff88
Jake Benilov <benilov@gmail.com>
2013-09-04 09:04:27 +0000

    break down exception stats by exception name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,9 @@ class ApplicationController < ActionController::Base
 
   protected
   def exception_notification_for(e)
+    exception_class_name = e.class.name.demodulize.downcase
+    $statsd.increment("#{::STATSD_PREFIX}.exception.#{exception_class_name}")
+
     if e.respond_to?(:errors)
       message = { data: { message: "Zendesk errors: #{e.errors}" } }
       ExceptionNotifier::Notifier.exception_notification(request.env, e, message).deliver
