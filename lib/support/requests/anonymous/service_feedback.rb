605bfeeaac39ba35663ad962b1ec259660d2c7b2
Jake Benilov <benilov@gmail.com>
2014-01-03 15:22:09 +0000

    Extend ServiceFeedback to calc aggregate stats

--- lib/support/requests/anonymous/service_feedback.rb
+++ lib/support/requests/anonymous/service_feedback.rb
@@ -10,6 +10,17 @@ module Support
         validates :details, length: { maximum: 2 ** 16 }
         validates_inclusion_of :service_satisfaction_rating, in: (1..5).to_a
         validates :url, url: true, length: { maximum: 2048 }, allow_nil: true
+
+        def self.aggregates_by_rating
+          zero_defaults = Hash[*(1..5).map {|n| [n, 0] }.flatten]
+          select("service_satisfaction_rating, count(*) as cnt").
+            group(:service_satisfaction_rating).
+            inject(zero_defaults) { |memo, result| memo[result[:service_satisfaction_rating]] = result[:cnt]; memo }
+        end
+
+        def self.with_comments_count
+          where("details IS NOT NULL").count
+        end
       end
     end
   end
--- test/unit/support/requests/anonymous/service_feedback_test.rb
+++ test/unit/support/requests/anonymous/service_feedback_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'date'
 require 'support/requests/anonymous/service_feedback'
 
 module Support
@@ -16,6 +17,39 @@ module Support
         should allow_value("http://" + ("a" * 2040)).for(:url)
         should_not allow_value("http://" + ("a" * 2050)).for(:url)
         should_not allow_value("http://bla.example.org:9292/méh/fào?bar").for(:url)
+
+        should "aggregate by rating" do
+          create_feedback(rating: 1)
+          create_feedback(rating: 2)
+          create_feedback(rating: 5)
+
+          expected = {
+            1 => 1,
+            2 => 1,
+            3 => 0,
+            4 => 0,
+            5 => 1
+          }
+          assert_equal expected, ServiceFeedback.aggregates_by_rating
+        end
+
+        should "aggregate by comment" do
+          create_feedback(rating: 1, details: "meh")
+          create_feedback(rating: 2)
+
+          assert_equal 1, ServiceFeedback.with_comments_count
+        end
+
+        def setup
+          ServiceFeedback.delete_all
+        end
+
+        def create_feedback(options)
+          ServiceFeedback.create!(slug: options[:slug] || "a",
+                                  details: options[:details],
+                                  service_satisfaction_rating: options[:rating],
+                                  javascript_enabled: true)
+        end
       end
     end
   end
