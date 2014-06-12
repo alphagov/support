5db80df458418fb3d13c8ff434c08e1112d0baa8
Jake Benilov <benilov@gmail.com>
2014-06-12 08:54:23 +0100

    Add missing require

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,3 +1,5 @@
+require 'support/requests/anonymous/service_feedback'
+
 FactoryGirl.define do
   factory :service_feedback, class: Support::Requests::Anonymous::ServiceFeedback do
     javascript_enabled true
