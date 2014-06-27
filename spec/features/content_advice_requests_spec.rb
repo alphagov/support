d509b2f74960008b21e00cd3bf6493b06e2f5522
Jake Benilov <benilov@gmail.com>
2014-06-27 09:13:15 +0000

    Content advice: capture title

--- spec/features/content_advice_requests_spec.rb
+++ spec/features/content_advice_requests_spec.rb
@@ -14,7 +14,7 @@ feature "Request for content advice" do
 
   scenario "successful request" do
     request = expect_zendesk_to_receive_ticket(
-      # "subject" => "Which format - Advice on content",
+      "subject" => "Which format - Advice on content",
       "tags" => [ "govt_form", "dept_content_advice" ],
 #       "comment" => { "body" =>
 # "[Nature of the request]
@@ -60,7 +60,7 @@ feature "Request for content advice" do
     click_on "Content advice"
     expect(page).to have_content("Ask for advice and guidance on Departments and Policy content")
 
-    # fill_in "Title of request", with: details[:title]
+    fill_in "Title of request", with: details[:title]
     # choose details[:nature_of_request]
     # fill_in "Details", with: details[:details]
     # fill_in "Relevant URLs (if applicable)", with: details[:urls]
