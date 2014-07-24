08aa141717b17db3e749d81bc81614d6b7c08c4a
Jake Benilov <benilov@gmail.com>
2014-07-24 08:45:32 +0000

    Port user removal request feature to spec

--- features/step_definitions/request_steps.rb
+++ features/step_definitions/request_steps.rb
@@ -83,23 +83,3 @@ When /^the user submits the following request to create or change users:$/ do |r
 
   step "the user submits the request successfully"
 end
-
-When /^the user submits the following remove user request:$/ do |request_details_table|
-  @request_details = request_details_table.hashes.first
-
-  visit '/'
-
-  click_on "Remove user"
-
-  assert page.has_content?("Request to remove user access")
-
-  within("#user_details") do
-    fill_in "Name", :with => @request_details["User's name"]
-    fill_in "Email", :with => @request_details["User's email"]
-    fill_in "Reason for removal", :with => @request_details["Reason for removal"]
-  end
-
-  fill_in "MUST NOT be removed BEFORE", :with => @request_details["Not before date"]
-
-  step "the user submits the request successfully"
-end
---
+++ spec/features/remove_user_requests_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+feature "Remove user requests" do
+  # In order to reduce the risk of a disgruntled ex-employee doing something damaging
+  # As a government employee responsible for a user of GDS tools
+  # I want to request to revoke the user's access (usually after they've left the org)
+
+  let(:user) { create(:user_manager, name: "John Smith", email: "john.smith@agency.gov.uk") }
+
+  background do
+    login_as user
+    zendesk_has_no_user_with_email(user.email)
+  end
+
+  scenario "successful request" do
+    request = expect_zendesk_to_receive_ticket(
+      "subject" => "Remove user",
+      "requester" => hash_including("name" => "John Smith", "email" => "john.smith@agency.gov.uk"),
+      "fields" => array_including("id" => GDSZendesk::FIELD_MAPPINGS[:not_before_date], "value" => "31-12-2020"),
+      "tags" => [ "govt_form", "remove_user" ],
+      "comment" => { "body" =>
+"[User name]
+Bob
+
+[User email]
+bob@someagency.gov.uk
+
+[Reason for removal]
+User has left the organisation"})
+
+    user_requests_removal_of_another_user(
+      user_name: "Bob",
+      user_email: "bob@someagency.gov.uk",
+      reason_for_removal: "User has left the organisation",
+      not_before_date: "31-12-2020"
+    )
+
+    expect(request).to have_been_made
+  end
+
+  private
+  def user_requests_removal_of_another_user(details)
+    visit '/'
+
+    click_on "Remove user"
+
+    expect(page).to have_content("Request to remove user access")
+
+    within("#user_details") do
+      fill_in "Name", with: details[:user_name]
+      fill_in "Email", with: details[:user_email]
+      fill_in "Reason for removal", with: details[:reason_for_removal]
+    end
+
+    fill_in "MUST NOT be removed BEFORE", with: details[:not_before_date]
+
+    user_submits_the_request_successfully
+  end
+end
