21bf8bb6ea773d99d929dbeb5e70496450e2313f
Jake Benilov <benilov@gmail.com>
2014-08-05 10:55:36 +0000

    Add breadcrumbs for feedex pages

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -6,3 +6,13 @@ crumb :request do |request|
   link request.class.label
   parent :root
 end
+
+crumb :feedex do
+  link "Feedback", anonymous_feedback_explore_path
+  parent :root
+end
+
+crumb :anonymous_feedback_by_path do |path|
+  link "Feedback for “#{path}”"
+  parent :feedex
+end
