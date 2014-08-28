4261caa25ea690c1fec260aa63ec8052ccbcda1d
Jake Benilov <benilov@gmail.com>
2014-08-28 20:50:18 +0000

    Remove code relating to service feedback stats upload
    This has been moved to the `support-api`.

--- lib/support/requests/anonymous/service_feedback.rb
+++ lib/support/requests/anonymous/service_feedback.rb
@@ -7,21 +7,6 @@ module Support
       class ServiceFeedback < AnonymousContact
         include ServiceFeedbackValidations
         attr_accessible :details, :slug, :service_satisfaction_rating
-
-        def self.transaction_slugs
-          uniq.pluck(:slug).sort
-        end
-
-        def self.aggregates_by_rating
-          zero_defaults = Hash[*(1..5).map {|n| [n, 0] }.flatten]
-          select("service_satisfaction_rating, count(*) as cnt").
-            group(:service_satisfaction_rating).
-            inject(zero_defaults) { |memo, result| memo[result[:service_satisfaction_rating]] = result[:cnt]; memo }
-        end
-
-        def self.with_comments_count
-          where("details IS NOT NULL").count
-        end
       end
     end
   end
