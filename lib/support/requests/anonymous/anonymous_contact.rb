7d368b5b54fff5843f3164a3b96ec24562788083
Jake Benilov <benilov@gmail.com>
2014-09-04 08:29:24 +0000

    Remove code relating to deduplication
    Deduplication has been moved to the `support-api`.

--- lib/support/requests/anonymous/anonymous_contact.rb
+++ lib/support/requests/anonymous/anonymous_contact.rb
@@ -1,5 +1,4 @@
 require 'support/requests/requester'
-require 'support/requests/anonymous/duplicate_detector'
 require 'support/requests/anonymous/with_personal_information_checking'
 require 'support/requests/anonymous/anonymous_contact_validations'
 
@@ -31,23 +30,6 @@ module Support
             only_actionable.
             order("created_at desc")
         }
-
-        def self.deduplicate_contacts_created_between(interval)
-          contacts = where(created_at: interval).order("created_at asc")
-          duplicate_detector = DuplicateDetector.new
-          contacts.each do |contact|
-            if duplicate_detector.duplicate?(contact)
-              contact.mark_as_duplicate
-              contact.save!
-            end
-          end
-        end
-
-        def mark_as_duplicate
-          self.is_actionable = false
-          self.reason_why_not_actionable = "duplicate"
-          self
-        end
 
         private
         def set_path_from_url
