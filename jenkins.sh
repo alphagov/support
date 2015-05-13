526f456a70bd1e481ae953aa4b26fd95fa1c4b25
Jake Benilov <benilov@gmail.com>
2015-05-13 23:24:59 +0100

    Remove DB manipulations from CI script

--- jenkins.sh
+++ jenkins.sh
@@ -5,5 +5,4 @@ set -e
 export RAILS_ENV=test
 git clean -fdx
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
-bundle exec rake db:drop db:create db:schema:load
 bundle exec rake --trace
