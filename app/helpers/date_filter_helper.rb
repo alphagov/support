24da8c1517716442d4b5452db2eec616982c7a59
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2015-05-19 14:22:49 +0000

    Responses header now displays dates selected
    ...when date filter being used

--- app/helpers/date_filter_helper.rb
+++ app/helpers/date_filter_helper.rb
@@ -1,7 +1,23 @@
 module DateFilterHelper
 
 	def date_filtered?
-		params[:from].present? || params[:to].present?
+		@from_date.present? or @to_date.present?
+	end
+
+	def total_responses_header(total_count, from, to)
+		response_total = pluralize(number_with_delimiter(total_count), 'response')
+
+		if from.present? and to.present?
+			"#{response_total} between #{from} and #{to}"
+		elsif from.present?
+			"#{response_total} since #{from}"
+		elsif to.present?
+			"#{response_total} before #{to}"
+		elsif total_count && total_count > 1
+			"All #{response_total}"
+		else
+			response_total
+		end
 	end
 
 end
