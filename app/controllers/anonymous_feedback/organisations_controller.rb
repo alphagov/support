e09a5921b966aac868f1af1e68f5d2c6feda55ae
Michael Patrick <michael.patrick@digital.cabinet-office.gov.uk>
2015-05-19 15:53:54 +0100

    WIP fetch organisation summary from API

--- app/controllers/anonymous_feedback/organisations_controller.rb
+++ app/controllers/anonymous_feedback/organisations_controller.rb
@@ -1,8 +1,22 @@
+require "gds_api/support_api"
+
 class AnonymousFeedback::OrganisationsController < AuthorisationController
   def show
     authorize! :read, :anonymous_feedback
 
-    # FIXME: actually get actual feedback based on organisation
-    @feedback = []
+    feedback = fetch_organisation_summary_from_support_api
+    @organisation_title = feedback["title"]
+    @content_items = feedback["results"].map { |content_item|
+      OpenStruct.new(content_item)
+    }.sort_by(&:path)
+  end
+
+private
+  def fetch_organisation_summary_from_support_api
+    support_api.organisation_summary(params[:slug])
+  end
+
+  def support_api
+    GdsApi::SupportApi.new(Plek.find("support-api"))
   end
 end
