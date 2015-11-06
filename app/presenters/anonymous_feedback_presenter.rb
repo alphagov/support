e69b1bfeff28d3fddac8d063072f16082a183eac
Paul Bowsher <paul.bowsher@gmail.com>
2015-11-06 14:34:49 +0000

    UI changes to support feedex result limiting.
    Uses the `results_limited` flag from support-api to display more
    accurate response counts if the user’s query has been truncated due to
    too many results. Also displays useful information on the last
    available page.

--- app/presenters/anonymous_feedback_presenter.rb
+++ app/presenters/anonymous_feedback_presenter.rb
@@ -1,7 +1,7 @@
 require 'ostruct'
 
 class AnonymousFeedbackPresenter < SimpleDelegator
-  attr_reader :current_page, :total_pages, :limit_value, :total_count
+  attr_reader :current_page, :total_pages, :limit_value, :total_count, :results_limited
 
   def initialize(api_response)
     # actually delegate to the API response's `results` array
@@ -11,6 +11,7 @@ class AnonymousFeedbackPresenter < SimpleDelegator
     @total_pages = api_response.pages
     @limit_value = api_response.page_size
     @total_count = api_response.total_count
+    @results_limited = api_response.results_limited if api_response.respond_to?(:results_limited)
   end
 
 private
