74d59133b0674c5b542b70fc2bb59113e339b620
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2016-03-15 14:26:10 +0000

    Harmonise hash rocket styles

--- spec/workers/zendesk_ticket_worker_spec.rb
+++ spec/workers/zendesk_ticket_worker_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe ZendeskTicketWorker do
   it "raises a ticket successfully" do
-    zendesk_has_user(email: "a@b.com", "suspended" => false)
+    zendesk_has_user(email: "a@b.com", suspended: false)
 
     stub = stub_zendesk_ticket_creation("some" => "options", "requester" => { "email" => "a@b.com" })
 
@@ -12,7 +12,7 @@ describe ZendeskTicketWorker do
   end
 
   it "doesn't raise a ticket if the user is suspended" do
-    zendesk_has_user(email: "a@b.com", "suspended" => true)
+    zendesk_has_user(email: "a@b.com", suspended: true)
 
     ZendeskTicketWorker.new.perform("some" => "options", "requester" => { "email" => "a@b.com" })
 
@@ -20,7 +20,7 @@ describe ZendeskTicketWorker do
   end
 
   it "discards the ticket if it receives a 409 response" do
-    zendesk_has_user(email: "a@b.com", "suspended" => false)
+    zendesk_has_user(email: "a@b.com", suspended: false)
 
     zendesk_returns_conflict
 
