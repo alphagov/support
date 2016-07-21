1ae45ac96437e1e0057cb8511c3bdde71cf59255
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2016-07-21 09:13:20 +0100

    Remove 'nature of request' inputs from 'content advice and help' form
    The form no longer has radio buttons for selecting the nature of the
    request. This feature is redundant.
    
    Also:
      - Removed associated label snippet

--- lib/support/requests/content_advice_request.rb
+++ lib/support/requests/content_advice_request.rb
@@ -7,39 +7,14 @@ module Support
     class ContentAdviceRequest < Request
       include WithTimeConstraint
 
-      attr_accessor :title, :nature_of_request, :nature_of_request_details,
-                    :details, :urls, :contact_number
+      attr_accessor :title, :details, :urls, :contact_number
 
-      validates_presence_of :nature_of_request
       validates_presence_of :details
-      validates :nature_of_request, inclusion: {
-        in: %w(initial_guidance formal_response other),
-        message: "%{value} is not a valid option"
-      }
-
-      validates_presence_of :nature_of_request_details,
-        if: Proc.new { |r| r.nature_of_request == 'other' }
 
       def initialize(attrs = {})
         self.time_constraint = TimeConstraint.new
 
         super
-      end
-
-      def nature_of_request_options
-        [
-          ["Initial guidance from GOV.UK on content you are working on", "initial_guidance"],
-          ["A formal response that you would like to pass onto other teams in your department or organisation", "formal_response"],
-          ["Other - please give information", "other"],
-        ]
-      end
-
-      def formatted_nature_of_request
-        if nature_of_request == "other"
-          nature_of_request_details
-        else
-          Hash[nature_of_request_options].key(nature_of_request)
-        end
       end
 
       def self.label
--- spec/models/support/requests/content_advice_request_spec.rb
+++ spec/models/support/requests/content_advice_request_spec.rb
@@ -7,19 +7,9 @@ module Support
       it { should validate_presence_of(:requester) }
       it { should validate_presence_of(:details) }
       it { should allow_value("xxx").for(:title) }
-      it { should allow_value("xxx").for(:nature_of_request_details) }
       it { should allow_value("xxx").for(:urls) }
 
-      it { should validate_presence_of(:nature_of_request) }
       it { should allow_value("xxx").for(:contact_number) }
-
-      its(:nature_of_request_options) { is_expected.to have_exactly(3).items }
-
-      context "nature of request is 'other'" do
-        subject { ContentAdviceRequest.new(nature_of_request: 'other') }
-
-        it { should validate_presence_of(:nature_of_request_details) }
-      end
 
       def as_str(date)
         date.strftime("%d-%m-%Y")
