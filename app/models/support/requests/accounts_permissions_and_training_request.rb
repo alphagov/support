d38495d32aa17980710cdc986c2419763e89737f
Murray Steele <muz@h-lame.com>
2017-11-23 17:07:37 +0000

    Inline WithUserNeeds into AccountsPermissionsandTrainingRequest
    This module is only used in this one place, so we can reduce the
    indirection without making the model significantly more complex by
    inlining the module into it.

--- app/models/support/requests/accounts_permissions_and_training_request.rb
+++ app/models/support/requests/accounts_permissions_and_training_request.rb
@@ -1,9 +1,10 @@
 module Support
   module Requests
     class AccountsPermissionsAndTrainingRequest < Request
-      include Support::GDS::WithUserNeeds
-
-      attr_accessor :action, :requested_user, :additional_comments
+      attr_accessor :action, :requested_user, :additional_comments,
+                    :user_needs, :mainstream_changes, :maslow,
+                    :other_details, :become_organisation_admin,
+                    :become_super_organisation_admin
 
       validate do |request|
         if request.requested_user && !request.requested_user.valid?
@@ -12,6 +13,7 @@ module Support
       end
       validates_presence_of :action, :requested_user
       validates :action, inclusion: { in: %w(create_new_user change_user) }
+      validates :formatted_user_needs, presence: { message: "must select at least one option" }
 
       def requested_user_attributes=(attr)
         self.requested_user = Support::GDS::RequestedUser.new(attr)
@@ -35,6 +37,38 @@ module Support
         [
           ["Create a new user account", "create_new_user"],
           ["Change an existing user's account", "change_user"],
+        ]
+      end
+
+      def formatted_user_needs
+        needs_list = []
+        needs_list << Hash[whitehall_account_options].key(user_needs)
+        needs_list << Hash[other_permissions_options].key("mainstream_changes") if self.mainstream_changes == "1"
+        needs_list << Hash[other_permissions_options].key("maslow") if self.maslow == "1"
+        needs_list << Hash[other_permissions_options].key("become_organisation_admin") if self.become_organisation_admin == "1"
+        needs_list << Hash[other_permissions_options].key("become_super_organisation_admin") if self.become_super_organisation_admin == "1"
+        needs_list << "Other: #{self.other_details}" if self.other_details.present?
+        needs_list.reject(&:blank?).compact.join("\n")
+      end
+
+      def inside_government_related?
+        %w{editor writer managing_editor}.include?(user_needs)
+      end
+
+      def whitehall_account_options
+        [
+          ["Writer - can create content", "writer"],
+          ["Editor - can create, review and publish content", "editor"],
+          ["Managing editor - can create, review and publish content, and has admin rights", "managing_editor"],
+        ]
+      end
+
+      def other_permissions_options
+        [
+          ["Request changes to your organisation’s mainstream content", "mainstream_changes"],
+          ["Access to Maslow database of user needs", "maslow"],
+          ["Request permission to be your organisation admin", "become_organisation_admin"],
+          ["Request permission to be a super organisation admin", "become_super_organisation_admin"],
         ]
       end
 
--- spec/models/support/requests/accounts_permissions_and_training_request_spec.rb
+++ spec/models/support/requests/accounts_permissions_and_training_request_spec.rb
@@ -36,6 +36,69 @@ module Support
         )
         expect(request).to have_at_least(1).error_on(:base)
       end
+
+      it "knows if it's related to inside government or not" do
+        expect(request(user_needs: "writer")).to be_inside_government_related
+        expect(request(user_needs: "editor")).to be_inside_government_related
+        expect(request(user_needs: "managing_editor")).to be_inside_government_related
+      end
+
+      describe "formatted_user_needs" do
+        context "for a Whitehall user account" do
+          it "returns the long text of the user need" do
+            expect(request(user_needs: "writer").formatted_user_needs).
+              to eq("Writer - can create content")
+          end
+        end
+
+        context "when nothing is selected" do
+          it "returns an empty string" do
+            expect(request(mainstream_changes: "0", maslow: "0").formatted_user_needs).to be_blank
+          end
+
+          it "fails validation" do
+            expect(request(mainstream_changes: "0", maslow: "0")).to_not be_valid
+          end
+        end
+
+        context "for other permissions" do
+          context "when one is ticked" do
+            it "returns the long text of the permission" do
+              expect(request(maslow: "1").formatted_user_needs).
+                to eq("Access to Maslow database of user needs")
+
+              expect(request(mainstream_changes: "1").formatted_user_needs).
+                  to eq("Request changes to your organisation’s mainstream content")
+
+              expect(request(become_organisation_admin: "1").formatted_user_needs).
+                  to eq("Request permission to be your organisation admin")
+
+              expect(request(become_super_organisation_admin: "1").formatted_user_needs).
+                  to eq("Request permission to be a super organisation admin")
+            end
+          end
+
+          context "when several are ticked" do
+            it "returns the long text of the permissions, with one permission per line" do
+              expect(request(mainstream_changes: "1", maslow: "1").formatted_user_needs).
+                to eq("Request changes to your organisation’s mainstream content\nAccess to Maslow database of user needs")
+            end
+          end
+
+          context "when other is filled in" do
+            it "returns the text of the other field" do
+              expect(request(other_details: "special permission request").formatted_user_needs).
+                to eq("Other: special permission request")
+            end
+            context "and when another permission is ticked" do
+              it "returns long text of the permissions and the text of the other field" do
+                expect(request(mainstream_changes: "1", other_details: "special permission request").formatted_user_needs).
+                  to eq("Request changes to your organisation’s mainstream content\nOther: special permission request")
+              end
+            end
+          end
+        end
+      end
     end
   end
 end
