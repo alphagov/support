75581e418d3533d9dcbe13c84ae8554e2f84d718
Elena Tanasoiu <elena.tanasoiu@gmail.com>
2018-02-01 22:53:30 +0000

    Updates list of fields for campaign
    Removes: title, other_dept_or_agency, success_measure
    Adds: type_of_site, has_read_guidance, has_read_oasis_guidance, development_start_date, performance_review_contact_email,
    government_theme, ga_contact_email

--- app/models/support/gds/campaign.rb
+++ app/models/support/gds/campaign.rb
@@ -4,14 +4,27 @@ module Support
   module GDS
     class Campaign
       include ActiveModel::Model
-      attr_accessor :title, :other_dept_or_agency, :signed_campaign, :start_date, :end_date, :description,
-                    :call_to_action, :success_measure, :proposed_url, :site_metadescription, :cost_of_campaign
+      attr_accessor :type_of_site, :has_read_guidance_confirmation, :has_read_oasis_guidance_confirmation,
+                    :signed_campaign, :start_date, :end_date, :development_start_date,
+                    :performance_review_contact_email, :government_theme, :description, :call_to_action,
+                    :proposed_url, :site_metadescription, :cost_of_campaign, :ga_contact_email
 
-      validates_presence_of :title, :signed_campaign, :start_date, :end_date, :description,
-                            :call_to_action, :success_measure, :proposed_url, :site_metadescription, :cost_of_campaign
-      validates_date :start_date, on_or_after: :today, on_or_before: :end_date
-      validates_date :end_date, on_or_after: :start_date
-      validates :proposed_url, format: /((http|https):\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.campaign.gov.uk?/
+      validates_presence_of :type_of_site, :signed_campaign, :start_date, :end_date, :development_start_date,
+                            :performance_review_contact_email, :government_theme, :description, :call_to_action,
+                            :proposed_url, :site_metadescription, :cost_of_campaign, :ga_contact_email
+
+      validates_acceptance_of :has_read_guidance_confirmation, :has_read_oasis_guidance_confirmation, allow_nil: false
+      validates_inclusion_of :type_of_site, in: ['Campaign platform', 'Bespoke microsite']
+
+      validates_date :start_date, on_or_after: :today, before: :end_date
+      validates_date :end_date, after: :start_date
+      validates_date :development_start_date, on_or_after: :start_date
+      validates :proposed_url, format: /((http|https):\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.(campaign\.)?gov.uk?/
+
+      validates_numericality_of :cost_of_campaign
+
+      VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+      validates :performance_review_contact_email, :ga_contact_email, format: { with: VALID_EMAIL_REGEX }
     end
   end
 end
