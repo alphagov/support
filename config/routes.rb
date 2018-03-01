8648b0f4c3ec92a757ed1c9234abf8f7b4045a96
Murray Steele <muz@h-lame.com>
2018-03-01 11:43:24 +0000

    Ran `bundle exec rake app:update`
    This gives us the new config and boilerplate for a rails 5.1 application
    and should make it easier to update to future versions as our application
    is as close to "default" as possible.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount GovukAdminTemplate::Engine, at: "/style-guide", as: "style_guide"
 
   Support::Navigation::SectionGroups.new.all_request_class_names.each do |request_class_name|
