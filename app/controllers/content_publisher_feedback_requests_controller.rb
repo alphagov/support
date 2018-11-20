9ab57410e28081be451b7ca8088a8749aa7895ee
Kevin Garwood <kevin.garwood@digital.cabinet-office.gov.uk>
2018-11-20 14:25:12 +0000

    Adding a feedback form for the content publisher application
    These changes relate to adding a new feedback form taht is hosted within this support app but
    which refers to activities users may encounter in the content-publisher app.  The form
    currently supports three main fields:
     - a required feedback_type option field
     - a required feedback_details field
     - an optional impact_on_work field
    
    All of the changes have conformed as much as possible to the way other pages have been added
    to the support app.

---
+++ app/controllers/content_publisher_feedback_requests_controller.rb
@@ -0,0 +1,24 @@
+class ContentPublisherFeedbackRequestsController < RequestsController
+protected
+
+  def new_request
+    Support::Requests::ContentPublisherFeedbackRequest.new
+  end
+
+  def zendesk_ticket_class
+    Zendesk::Ticket::ContentPublisherFeedbackRequestTicket
+  end
+
+  def parse_request_from_params
+    Support::Requests::ContentPublisherFeedbackRequest.new(content_publisher_feedback_request_params)
+  end
+
+  def content_publisher_feedback_request_params
+    params.require(:support_requests_content_publisher_feedback_request).permit(
+      :feedback_type,
+      :feedback_details,
+      :impact_on_work,
+      requester_attributes: %i[email name collaborator_emails],
+    ).to_h
+  end
+end
---
+++ app/models/support/requests/content_publisher_feedback_request.rb
@@ -0,0 +1,31 @@
+require 'active_support/core_ext'
+
+module Support
+  module Requests
+    class ContentPublisherFeedbackRequest < Request
+      attr_accessor :feedback_type, :feedback_details, :impact_on_work
+
+      OPTIONS = {
+        "accessibility or usability" => "accessibility or usability",
+        "working unexpectedly" => "something working differently than you expected",
+        "helpful feature" => "a feature that would help you with your work",
+        "other" => "something else",
+      }.freeze
+
+      validates_presence_of :feedback_type, :feedback_details
+      validates :feedback_type, inclusion: { in: OPTIONS.keys }
+
+      def self.label
+        "Give feedback on Content Publisher (Beta)"
+      end
+
+      def self.description
+        "Suggest changes to features within Content Publisher."
+      end
+
+      def feedback_type_options
+        OPTIONS.map { |key, value| [value, key] }
+      end
+    end
+  end
+end
