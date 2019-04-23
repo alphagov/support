0dbd62163988d29b3fe3b2d316692f82d9ff5f08
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-04-23 13:12:24 +0100

    Make autoloading modules work better

---
+++ app/models/support/requests.rb
@@ -0,0 +1,1 @@
+module Support::Requests; end
--- app/models/support/requests/anonymous/paths.rb
+++ app/models/support/requests/anonymous/paths.rb
@@ -1,35 +1,33 @@
 require "redis_client"
 
-module Support::Requests::Anonymous
-  class Paths
-    EXPIRATION_TTL = 7.days
+class Support::Requests::Anonymous::Paths
+  EXPIRATION_TTL = 7.days
 
-    def initialize(paths, id = nil)
-      @id = id || SecureRandom.hex(10)
-      @paths = paths
-    end
+  def initialize(paths, id = nil)
+    @id = id || SecureRandom.hex(10)
+    @paths = paths
+  end
 
-    attr_reader :id, :paths
+  attr_reader :id, :paths
 
-    def save
-      key = Paths.redis_key(id: id)
-      Paths.redis.setex(key, EXPIRATION_TTL, paths.to_json)
-    end
+  def save
+    key = self.class.redis_key(id: id)
+    self.class.redis.setex(key, EXPIRATION_TTL, paths.to_json)
+  end
 
-    def self.find(id)
-      key = redis_key(id: id)
-      paths = JSON.parse(redis.get(key))
-      Paths.new(paths, id)
-    rescue TypeError
-      nil
-    end
+  def self.find(id)
+    key = redis_key(id: id)
+    paths = JSON.parse(redis.get(key))
+    new(paths, id)
+  rescue TypeError
+    nil
+  end
 
-    def self.redis_key(id:)
-      "anonymous-paths:#{id}"
-    end
+  def self.redis_key(id:)
+    "anonymous-paths:#{id}"
+  end
 
-    def self.redis
-      RedisClient.instance.connection
-    end
+  def self.redis
+    RedisClient.instance.connection
   end
 end
