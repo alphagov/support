065e1596eb401806530a4e85cc4b70a15b9d4a7d
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-10-01 10:32:45 +0100

    Auto-fix linting issues

--- config/initializers/gds_zendesk.rb
+++ config/initializers/gds_zendesk.rb
@@ -10,6 +10,6 @@ else
   ZENDESK_CREDENTIALS = {
     "username" => ENV["ZENDESK_CLIENT_USERNAME"] || "username",
     "password" => ENV["ZENDESK_CLIENT_PASSWORD"] || "password",
-  }
+  }.freeze
   GDS_ZENDESK_CLIENT = GDSZendesk::Client.new(username: ZENDESK_CREDENTIALS["username"], password: ZENDESK_CREDENTIALS["password"], logger: Rails.logger)
 end
