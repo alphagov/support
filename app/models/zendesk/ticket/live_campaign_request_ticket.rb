6cefefe0f03e8320b897eecb7f51cb8cbb4689fe
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-05-06 09:25:56 +0100

    Linting auto-correct

--- app/models/zendesk/ticket/live_campaign_request_ticket.rb
+++ app/models/zendesk/ticket/live_campaign_request_ticket.rb
@@ -13,16 +13,16 @@ module Zendesk
 
       def comment_snippets
         [
-            Zendesk::LabelledSnippet.new(on: @request.live_campaign, field: :title,
-                                         label: "Campaign Title"),
-            Zendesk::LabelledSnippet.new(on: @request.live_campaign, field: :proposed_url,
-                                         label: "Campaign URL"),
-            Zendesk::LabelledSnippet.new(on: @request.live_campaign, field: :description,
-                                         label: "Details of requested support"),
-            Zendesk::LabelledSnippet.new(on: @request.live_campaign, field: :time_constraints,
-                                         label: "Are there any time constraints for this request?"),
-            Zendesk::LabelledSnippet.new(on: @request.live_campaign, field: :reason_for_dates,
-                                         label: "Reason for the above dates?"),
+          Zendesk::LabelledSnippet.new(on: @request.live_campaign, field: :title,
+                                       label: "Campaign Title"),
+          Zendesk::LabelledSnippet.new(on: @request.live_campaign, field: :proposed_url,
+                                       label: "Campaign URL"),
+          Zendesk::LabelledSnippet.new(on: @request.live_campaign, field: :description,
+                                       label: "Details of requested support"),
+          Zendesk::LabelledSnippet.new(on: @request.live_campaign, field: :time_constraints,
+                                       label: "Are there any time constraints for this request?"),
+          Zendesk::LabelledSnippet.new(on: @request.live_campaign, field: :reason_for_dates,
+                                       label: "Reason for the above dates?"),
         ]
       end
     end
