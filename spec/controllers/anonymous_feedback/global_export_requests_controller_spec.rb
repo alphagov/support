54eafe230ca6cc0e7210c7486e37fe74a0f54fe8
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-05-12 11:37:37 +0100

    Fix bug with 'exclude_spam' always set to 'true'

--- spec/controllers/anonymous_feedback/global_export_requests_controller_spec.rb
+++ spec/controllers/anonymous_feedback/global_export_requests_controller_spec.rb
@@ -16,27 +16,41 @@ describe AnonymousFeedback::GlobalExportRequestsController, type: :controller do
   before { login_as user }
 
   describe "#create" do
-    let!(:stub_request) do
-      stub_support_api_global_export_request_creation(
+    let(:stub_params) do
+      {
         notification_email: user.email,
         from_date: "1 Aug 2016",
         to_date: "8 Aug 2016",
-        exclude_spam: true,
-      )
+      }
     end
 
-    it "makes a successful create request" do
-      post :create, params: { from_date: "1 Aug 2016", to_date: "8 Aug 2016", exclude_spam: "1" }
-      expect(stub_request).to have_been_made
+    before do
+      stub_support_api_global_export_request_creation(exclude_spam: true, **stub_params)
+    end
+
+    context "excluding spam" do
+      it "makes a successful create request" do
+        stub_request = stub_support_api_global_export_request_creation(exclude_spam: true, **stub_params)
+        post :create, params: { from_date: "1 Aug 2016", to_date: "8 Aug 2016", exclude_spam: "1" }
+        expect(stub_request).to have_been_made
+      end
+    end
+
+    context "including spam" do
+      it "makes a successful create request" do
+        stub_request = stub_support_api_global_export_request_creation(exclude_spam: false, **stub_params)
+        post :create, params: { from_date: "1 Aug 2016", to_date: "8 Aug 2016" }
+        expect(stub_request).to have_been_made
+      end
     end
 
     it "sets the flash" do
-      post :create, params: { from_date: "1 Aug 2016", to_date: "8 Aug 2016" }
+      post :create, params: { from_date: "1 Aug 2016", to_date: "8 Aug 2016", exclude_spam: "1" }
       expect(flash[:notice]).to include "foo.bar@example.gov.uk"
     end
 
     it "redirects to the feedback explore page" do
-      post :create, params: { from_date: "1 Aug 2016", to_date: "8 Aug 2016" }
+      post :create, params: { from_date: "1 Aug 2016", to_date: "8 Aug 2016", exclude_spam: "1" }
       expect(response).to redirect_to(anonymous_feedback_explore_path)
     end
   end
