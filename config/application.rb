21bea2f2025b8a10633fec124f384bc05e4450fb
Ben <benbaumann95@hotmail.co.uk>
2020-08-03 16:03:00 +0100

    Switch to use load_defaults 6.0 - Override config in application.rb - Remove new_framework_defaults.rb files
    Add GDS acronym inflection for Zeitwerk autoloader
    
    Switch config.cache_classes from true to false

--- config/application.rb
+++ config/application.rb
@@ -17,10 +17,23 @@ Bundler.require(*Rails.groups)
 module Support
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    # Enable per-form CSRF tokens.
+    config.action_controller.per_form_csrf_tokens = false
+
+    # Enable origin-checking CSRF mitigation.
+    config.action_controller.forgery_protection_origin_check = false
+
+    # Make Ruby 2.4 preserve the timezone of the receiver when calling `to_time`.
+    ActiveSupport.to_time_preserves_timezone = false
+
+    # Make `form_with` generate non-remote forms.
+    config.action_view.form_with_generates_remote_forms = false
   end
 end
