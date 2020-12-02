5e92fcc5a5eb7549a9cdb17a60ca620d24023c60
Kevin Dew <kevindew@me.com>
2020-12-02 00:33:53 +0000

    Remove jquery require to resolve JS errors
    JQuery is already available due to govuk_admin_template. Requiring it
    here installs it a second time which causes conflicts.
    
    Removing this resolves two JS errors:
    
    - Uncaught TypeError: e.datepicker is not a function
    - Uncaught TypeError: $(...).dropdown is not a function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require jquery-ui/widgets/datepicker
 //= require_directory ./modules
 //= require select2
