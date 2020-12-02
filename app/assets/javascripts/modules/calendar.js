e8066393dc4622dc62e993e086cfd7d63ea720c6
Kevin Dew <kevindew@me.com>
2020-12-02 00:10:05 +0000

    Fix JS linting issues

--- app/assets/javascripts/modules/calendar.js
+++ app/assets/javascripts/modules/calendar.js
@@ -1,30 +1,29 @@
-(function(Modules) {
-  "use strict";
+(function (Modules) {
+  'use strict'
   /*
      Initialise a jQuery UI date picker
      https://jqueryui.com/datepicker/
   */
-  Modules.Calendar = function() {
-    this.start = function(element) {
-
+  Modules.Calendar = function () {
+    this.start = function (element) {
       // Date format: https://jqueryui.com/datepicker/#date-formats
       // Min and max dates: https://jqueryui.com/datepicker/#min-max
       var options = {
-            dateFormat: element.data('format') || 'd M yy'
-          },
-          maxDate = element.data('max-date'),
-          minDate = element.data('min-date');
+        dateFormat: element.data('format') || 'd M yy'
+      }
+      var maxDate = element.data('max-date')
+      var minDate = element.data('min-date')
 
-      if (typeof maxDate === "number") {
-        options.maxDate = maxDate;
+      if (typeof maxDate === 'number') {
+        options.maxDate = maxDate
       }
 
-      if (typeof minDate === "number") {
-        options.minDate = minDate;
+      if (typeof minDate === 'number') {
+        options.minDate = minDate
       }
 
       // Create jQuery UI date picker
-      element.datepicker(options);
+      element.datepicker(options)
     }
-  };
-})(window.GOVUKAdmin.Modules);
+  }
+})(window.GOVUKAdmin.Modules)
