efbfb03a42d1cdf5093fd05ba170a891bf2e2a5b
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2023-02-03 14:56:06 +0000

    Update time field labels

--- spec/support/app_actions.rb
+++ spec/support/app_actions.rb
@@ -77,9 +77,9 @@ module AppActions
   end
 
   def user_fills_out_time_constraints(details)
-    fill_in "MUST be published by", with: details[:needed_by_date]
-    fill_in "MUST NOT be published BEFORE", with: details[:not_before_date]
-    fill_in "Reason for the above dates", with: details[:reason]
+    fill_in "Deadline (if you have one)", with: details[:needed_by_date]
+    fill_in "Must not be published before", with: details[:not_before_date]
+    fill_in "Reason for deadline", with: details[:reason]
   end
 
   def user_submits_the_request_successfully
