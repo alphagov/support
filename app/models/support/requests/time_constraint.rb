f3a05a7b6081adaa206f6a7abf5ce2acc8fd42f6
Mahmud Hussain <mahmud.hussain@digital.cabinet-office.gov.uk>
2023-04-12 16:20:44 +0100

    Validate times and radio button - Checks if the date and time fields are in the future or past - Checks radio button is yes or no

--- app/models/support/requests/time_constraint.rb
+++ app/models/support/requests/time_constraint.rb
@@ -4,11 +4,27 @@ module Support
   module Requests
     class TimeConstraint
       include ActiveModel::Model
-      attr_accessor :not_before_date, :not_before_time, :needed_by_date, :needed_by_time, :time_constraint_reason
+      attr_accessor :not_before_date, :not_before_time, :needed_by_date, :needed_by_time, :time_constraint_reason, :is_legal_deadline
+
+      validates :is_legal_deadline, inclusion: %w[yes no], allow_blank: true
 
       validates_date :needed_by_date, allow_nil: true, allow_blank: true, on_or_after: :today
       validates_date :not_before_date, allow_nil: true, allow_blank: true, on_or_after: :today
       validates_date :not_before_date, on_or_before: :needed_by_date, unless: proc { |c| c.needed_by_date.blank? || c.not_before_date.blank? }
+
+      validates_time :not_before_time, allow_blank: true
+      validates_time :needed_by_time, allow_blank: true
+
+      validates_time :not_before_time, on_or_after: :now, unless: proc { |c|
+        c.not_before_time.blank? || c.not_before_date != Time.zone.today.strftime("%d-%m-%Y")
+      }
+      validates_time :needed_by_time, on_or_after: :now, unless: proc { |c|
+        c.needed_by_time.blank? || c.needed_by_date != Time.zone.today.strftime("%d-%m-%Y")
+      }
+
+      validates_time :not_before_time, on_or_before: :needed_by_time, unless: proc { |c|
+        [c.needed_by_date, c.needed_by_time, c.not_before_date, c.not_before_time].any?(&:blank?) || c.needed_by_date != c.not_before_date
+      }
     end
   end
 end
--- spec/models/support/requests/time_constraint_spec.rb
+++ spec/models/support/requests/time_constraint_spec.rb
@@ -7,6 +7,10 @@ module Support
         date.strftime("%d-%m-%Y")
       end
 
+      def time_as_str(time)
+        time.strftime("%H:%M")
+      end
+
       it { should_not allow_value("xxx").for(:needed_by_date) }
 
       it { should allow_value(as_str(Date.tomorrow)).for(:needed_by_date) }
@@ -32,6 +36,57 @@ module Support
         expect(constraint).to have_at_least(1).error_on(:not_before_date)
       end
 
+      it "doesn't allow the 'not before' time to be set after the 'needed by' time, if the 'not before' date and 'not before' time are the same" do
+        constraint = TimeConstraint.new(
+          not_before_date: as_str(Date.tomorrow),
+          needed_by_date: as_str(Date.tomorrow),
+          not_before_time: "12:00",
+          needed_by_time: "11:00",
+        )
+        expect(constraint).to_not be_valid
+        expect(constraint).to have_at_least(1).error_on(:not_before_time)
+      end
+
+      it "allows the 'not before' time to be set after the 'needed by' time, if the 'not before' date and 'not before' time are different" do
+        constraint = TimeConstraint.new(
+          not_before_date: as_str(Date.tomorrow),
+          needed_by_date: as_str(Date.tomorrow + 1.day),
+          not_before_time: "12:00",
+          needed_by_time: "11:00",
+        )
+        expect(constraint).to be_valid
+      end
+
+      it "validates that the 'not before' time is after now" do
+        constraint = TimeConstraint.new(
+          not_before_date: as_str(Time.zone.today),
+          not_before_time: time_as_str(Time.zone.now - 1.minute),
+        )
+        expect(constraint).to_not be_valid
+        expect(constraint).to have_at_least(1).error_on(:not_before_time)
+
+        constraint = TimeConstraint.new(
+          not_before_date: as_str(Date.tomorrow),
+          not_before_time: time_as_str(Time.zone.now - 1.minute),
+        )
+        expect(constraint).to be_valid
+      end
+
+      it "validates that the 'needed by' time is after now" do
+        constraint = TimeConstraint.new(
+          needed_by_date: as_str(Time.zone.today),
+          needed_by_time: time_as_str(Time.zone.now - 1.minute),
+        )
+        expect(constraint).to_not be_valid
+        expect(constraint).to have_at_least(1).error_on(:needed_by_time)
+
+        constraint = TimeConstraint.new(
+          needed_by_date: as_str(Time.zone.tomorrow),
+          needed_by_time: time_as_str(Time.zone.now - 1.minute),
+        )
+        expect(constraint).to be_valid
+      end
+
       it "allows a blank not_before_date if the needed_by_date is set" do
         expect(TimeConstraint.new(needed_by_date: as_str(Date.tomorrow))).to be_valid
       end
@@ -42,6 +97,22 @@ module Support
           needed_by_date: as_str(Date.tomorrow),
         )
         expect(constraint).to be_valid
+      end
+
+      it "validates time formats" do
+        %i[needed_by_time not_before_time].each do |attribute|
+          should allow_value("14:00").for(attribute)
+          should_not allow_value("25:00").for(attribute)
+          should_not allow_value("14:60").for(attribute)
+          should_not allow_value("14").for(attribute)
+          should_not allow_value("1400").for(attribute)
+          should_not allow_value("14:00 PM").for(attribute)
+          should_not allow_value("2 : 00").for(attribute)
+          # These values should never happen in practice because the dropdown menu uses 24 hour time,
+          # but they're still valid
+          should allow_value("2:00PM").for(attribute)
+          should allow_value("2:00pm").for(attribute)
+        end
       end
     end
   end
