bcd9984967f70e5338b21455debb3ecd72684de3
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2023-10-13 13:08:18 +0100

    Temporarily add content request fields into the ticket body
    There is an issue with Zapier integration the content teams use to create
    Trello cards from Zendesk tickets.  Deadlines/not-before dates aren’t pulling
    through on cards because this data is now in different fields. This was
    anticipated however colleagues with with login details to the Zapier account
    are not available today to make a change.  We will temporally add this
    information to the ticket body so it's copied over to Trello with Zapier.
    
    Once Zapier is updated we can revert this commit.

--- spec/features/content_change_requests_spec.rb
+++ spec/features/content_change_requests_spec.rb
@@ -20,7 +20,25 @@ feature "Content change requests" do
       "tags" => %w[govt_form content_amend],
       "comment" => {
         "body" =>
-"[URLs to be changed]
+"[Reason for request]
+Factual inaccuracy
+
+[Subject area]
+Benefits
+
+[Deadline date]
+31-12-#{next_year}
+
+[Deadline time]
+13:00
+
+[Do not publish before date]
+01-12-#{next_year}
+
+[Do not publish before time]
+18:00
+
+[URLs to be changed]
 http://gov.uk/X
 
 [Details of what should be added, amended or removed]
