cec56ca818824b1c6ee9e5d10bf6288d5f6b0efa
James Mead <james.mead@digital.cabinet-office.gov.uk>
2024-01-11 15:31:14 +0000

    Remove/change irrelevant examples from existing user requests controller
    This controller is now only concerned with requests to change existing
    users; not requests to create new users. So I've changed one example to
    make it more realistic and remove two that are no longer relevant.

--- spec/controllers/change_existing_user_requests_controller_spec.rb
+++ spec/controllers/change_existing_user_requests_controller_spec.rb
@@ -8,17 +8,6 @@ describe ChangeExistingUserRequestsController, type: :controller do
     }
   end
 
-  def valid_create_user_request_params
-    {
-      "support_requests_change_existing_user_request" => {
-        "requester_attributes" => valid_requester_params,
-        "requested_user_attributes" => valid_requested_user_params,
-        "action" => "create_new_user",
-        "additional_comments" => "",
-      },
-    }
-  end
-
   def valid_change_user_request_params
     {
       "support_requests_change_existing_user_request" => {
@@ -39,46 +28,15 @@ describe ChangeExistingUserRequestsController, type: :controller do
   end
 
   context "submitted user creation request" do
-    it "submits the request to Zendesk and creates a Zendesk user with the requested user details" do
-      zendesk_has_no_user_with_email(valid_requested_user_params["email"])
+    it "submits the request to Zendesk" do
       stub_ticket_creation = stub_zendesk_ticket_creation(
-        hash_including("tags" => %w[govt_form create_new_user]),
+        hash_including("tags" => %w[govt_form change_user]),
       )
-      stub_user_creation = stub_zendesk_user_creation(
-        email: "subject@digital.cabinet-office.gov.uk",
-        name: "subject",
-        details: "Job title: ",
-        phone: nil,
-        verified: true,
-      )
-
-      post :create, params: valid_create_user_request_params
-
-      expect(request).to redirect_to("/acknowledge")
-      expect(stub_ticket_creation).to have_been_made
-      expect(stub_user_creation).to have_been_made
-    end
-
-    it "doesn't make any changes to the Zendesk user for change user requests" do
-      zendesk_ticket_request = stub_zendesk_ticket_creation
 
       post :create, params: valid_change_user_request_params
 
-      expect(response).to redirect_to("/acknowledge")
-      expect(zendesk_ticket_request).to have_been_made
-    end
-
-    it "doesn't expose an error to the user when automatic user creation goes wrong" do
-      zendesk_is_unavailable
-
-      allow_any_instance_of(Zendesk::ZendeskTickets).to receive(:raise_ticket)
-
-      expect(GovukError).to receive(:notify)
-        .with(kind_of(ZendeskAPI::Error::ClientError))
-
-      post :create, params: valid_create_user_request_params
-
-      expect(response).to redirect_to("/acknowledge")
+      expect(request).to redirect_to("/acknowledge")
+      expect(stub_ticket_creation).to have_been_made
     end
   end
 end
