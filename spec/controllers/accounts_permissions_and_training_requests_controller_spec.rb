3e3e063cc0304ab62513246e9e654f3ab3689925
James Mead <james.mead@digital.cabinet-office.gov.uk>
2024-01-11 15:47:15 +0000

    Remove redundant context from new user requests controller spec
    This controller is now only concerned with requests to create new users;
    not requests to change existing users. And so this context is redundant
    and can be removed.
    
    The diff is best viewed using the `ignore-all-space` git option, because
    of the indentation changes.

--- spec/controllers/accounts_permissions_and_training_requests_controller_spec.rb
+++ spec/controllers/accounts_permissions_and_training_requests_controller_spec.rb
@@ -24,38 +24,36 @@ describe AccountsPermissionsAndTrainingRequestsController, type: :controller do
     zendesk_has_no_user_with_email(@user.email)
   end
 
-  context "submitted user creation request" do
-    it "submits the request to Zendesk and creates a Zendesk user with the requested user details" do
-      zendesk_has_no_user_with_email(valid_requested_user_params["email"])
-      stub_ticket_creation = stub_zendesk_ticket_creation(
-        hash_including("tags" => %w[govt_form create_new_user]),
-      )
-      stub_user_creation = stub_zendesk_user_creation(
-        email: "subject@digital.cabinet-office.gov.uk",
-        name: "subject",
-        details: "Job title: ",
-        phone: nil,
-        verified: true,
-      )
+  it "submits the request to Zendesk and creates a Zendesk user with the requested user details" do
+    zendesk_has_no_user_with_email(valid_requested_user_params["email"])
+    stub_ticket_creation = stub_zendesk_ticket_creation(
+      hash_including("tags" => %w[govt_form create_new_user]),
+    )
+    stub_user_creation = stub_zendesk_user_creation(
+      email: "subject@digital.cabinet-office.gov.uk",
+      name: "subject",
+      details: "Job title: ",
+      phone: nil,
+      verified: true,
+    )
 
-      post :create, params: valid_create_user_request_params
+    post :create, params: valid_create_user_request_params
 
-      expect(request).to redirect_to("/acknowledge")
-      expect(stub_ticket_creation).to have_been_made
-      expect(stub_user_creation).to have_been_made
-    end
+    expect(request).to redirect_to("/acknowledge")
+    expect(stub_ticket_creation).to have_been_made
+    expect(stub_user_creation).to have_been_made
+  end
 
-    it "doesn't expose an error to the user when automatic user creation goes wrong" do
-      zendesk_is_unavailable
+  it "doesn't expose an error to the user when automatic user creation goes wrong" do
+    zendesk_is_unavailable
 
-      allow_any_instance_of(Zendesk::ZendeskTickets).to receive(:raise_ticket)
+    allow_any_instance_of(Zendesk::ZendeskTickets).to receive(:raise_ticket)
 
-      expect(GovukError).to receive(:notify)
-        .with(kind_of(ZendeskAPI::Error::ClientError))
+    expect(GovukError).to receive(:notify)
+      .with(kind_of(ZendeskAPI::Error::ClientError))
 
-      post :create, params: valid_create_user_request_params
+    post :create, params: valid_create_user_request_params
 
-      expect(response).to redirect_to("/acknowledge")
-    end
+    expect(response).to redirect_to("/acknowledge")
   end
 end
