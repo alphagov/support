5bbe95af9edae6098ada690cc1ae32e62aff6aa9
PeterHattyar <peter.hattyar@digital.cabinet-office.gov.uk>
2024-08-20 15:04:50 +0100

    Breaking up the Campaign Request Form Date Inputs
    Date picker arrows in Support app stopped working after the migration to Dart Sass. It’s caused by a known issue in the way Dart Sass handles the icon as Unicode - sass/sass#1395 we were not able to find a workaround. Date picker are not very accessible. When users are filling the dates in the form they normally know them already (and don’t need to pick it by looking at a calendar).
    
    Users see a  symbol instead of an arrow. There’s alt text (Next, Prev). It’s also possible to enter date manually.
    
    This will allow us to drop dependency on jquery-ui-rails.
    
    Due to delivery priorities, we're not converting the entire form into Design System at the moment.

--- app/models/support/gds/campaign.rb
+++ app/models/support/gds/campaign.rb
@@ -4,15 +4,22 @@ module Support
   module GDS
     class Campaign
       include ActiveModel::Model
+      include DesignSystemDateHelper
       attr_accessor :has_read_guidance_confirmation,
                     :has_read_oasis_guidance_confirmation,
                     :full_responsibility_confirmation,
                     :accessibility_confirmation,
                     :cookie_and_privacy_notice_confirmation,
                     :signed_campaign,
-                    :start_date,
-                    :end_date,
-                    :development_start_date,
+                    :start_day,
+                    :start_month,
+                    :start_year,
+                    :end_day,
+                    :end_month,
+                    :end_year,
+                    :development_start_day,
+                    :development_start_month,
+                    :development_start_year,
                     :performance_review_contact_email,
                     :government_theme,
                     :description,
@@ -27,9 +34,15 @@ module Support
                     :ga_contact_email
 
       validates :signed_campaign,
-                :start_date,
-                :end_date,
-                :development_start_date,
+                :start_day,
+                :start_month,
+                :start_year,
+                :end_day,
+                :end_month,
+                :end_year,
+                :development_start_day,
+                :development_start_month,
+                :development_start_year,
                 :performance_review_contact_email,
                 :government_theme,
                 :description,
@@ -50,7 +63,6 @@ module Support
                 :accessibility_confirmation,
                 :cookie_and_privacy_notice_confirmation,
                 acceptance: { allow_nil: false, accept: "Yes" }
-
       validates_date :start_date, on_or_after: :today
       validates_date :end_date, after: :start_date
       validates_date :development_start_date, on_or_before: :start_date
@@ -58,6 +70,18 @@ module Support
 
       VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
       validates :performance_review_contact_email, :ga_contact_email, format: { with: VALID_EMAIL_REGEX }
+
+      def start_date
+        formatted_date(start_day, start_month, start_year)
+      end
+
+      def end_date
+        formatted_date(end_day, end_month, end_year)
+      end
+
+      def development_start_date
+        formatted_date(development_start_day, development_start_month, development_start_year)
+      end
     end
   end
 end
