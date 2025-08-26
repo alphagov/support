4a638934531eee50cf362cc3c330f1dcb06cd8ba
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-08-26 19:18:58 +0100

    Switch to using load-analytics to load GTM
    This brings Support into line with Whitehall
    (https://github.com/alphagov/whitehall/pull/8941) and Signon
    (https://github.com/alphagov/signon/pull/4006/commits/a23dfeacbd51d975d727244c40385eb8d7d5c8a5),
    resolving an issue where we weren't reporting page views (because our
    GTM is no longer configured to send these by defaults), or populating
    the data layer.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,6 @@
 //= require govuk_publishing_components/dependencies
+//= require govuk_publishing_components/lib/cookie-functions
+//= require ./domain-config
+
+window.GOVUK.approveAllCookieTypes()
+window.GOVUK.cookie('cookies_preferences_set', 'true', { days: 365 })
