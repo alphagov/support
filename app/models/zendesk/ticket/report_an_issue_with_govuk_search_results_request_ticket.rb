cf454e042491fff119189c4d91f3e63822603fd0
Emma Lowe <emma.lowe@digital.cabinet-office.gov.uk>
2026-07-07 16:09:19 +0100

    Make search query question clearer
    To better reflect that requests are raised on behalf of other users.

--- app/models/zendesk/ticket/report_an_issue_with_govuk_search_results_request_ticket.rb
+++ app/models/zendesk/ticket/report_an_issue_with_govuk_search_results_request_ticket.rb
@@ -13,7 +13,7 @@ module Zendesk
 
       def comment_snippets
         [
-          Zendesk::LabelledSnippet.new(on: @request, field: :search_query, label: "What search query did you use?"),
+          Zendesk::LabelledSnippet.new(on: @request, field: :search_query, label: "What search queries are not working well?"),
           Zendesk::LabelledSnippet.new(on: @request, field: :results_problem, label: "What is the problem with the search results?"),
           Zendesk::LabelledSnippet.new(on: @request, field: :change_requested, label: "What change do you want to make to the search results?"),
           Zendesk::LabelledSnippet.new(on: @request, field: :change_justification, label: "Why is this change necessary?"),
--- spec/features/report_an_issue_with_govuk_search_results_spec.rb
+++ spec/features/report_an_issue_with_govuk_search_results_spec.rb
@@ -13,7 +13,7 @@ feature "Report an issue with GOV.UK search results" do
       "requester" => hash_including("name" => "John Smith", "email" => "john.smith@email.co.uk"),
       "tags" => %w[govt_form site_search],
       "description" =>
-"[What search query did you use?]
+"[What search queries are not working well?]
 search-query
 
 [What is the problem with the search results?]
@@ -42,7 +42,7 @@ private
     visit "/"
     click_on "Report an issue with GOV.UK search results"
     expect(page).to have_content("Report an issue with GOV.UK search results")
-    fill_in "What search query did you use, for example “Universal Credit”?", with: details[:search_query]
+    fill_in "What search queries are not working well?", with: details[:search_query]
     fill_in "What is the problem with the search results?", with: details[:results_problem]
     fill_in "What change do you want to make to the search results? Include URLs for any pages you want the search to show.", with: details[:change_requested]
     fill_in "Explain why this change is necessary, including the impact on users and the number of users affected.", with: details[:change_justification]
